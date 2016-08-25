c8a383943d3db0f30a37bb16b81690e7b7c1fa4e
viKuo <lost.angel.fin@gmail.com>
2016-08-25 09:39:24 -0400

    create arrayadapter for filter spinners

--- app/src/main/java/com/vivienlk/wardrobeinventory/UI/FilterItemsFragment.java
+++ app/src/main/java/com/vivienlk/wardrobeinventory/UI/FilterItemsFragment.java
@@ -5,9 +5,11 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 
 import com.vivienlk.wardrobeinventory.R;
+import com.vivienlk.wardrobeinventory.models.WardrobeItem;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -17,8 +19,8 @@ import butterknife.ButterKnife;
  */
 public class FilterItemsFragment extends Fragment {
     @BindView(R.id.itemFilterSpinner) Spinner mItemSpinner;
-    @BindView(R.id.itemFilterSpinner) Spinner mColorSpinner;
-    @BindView(R.id.itemFilterSpinner) Spinner mSeasonSpinner;
+    @BindView(R.id.colorFilterSpinner) Spinner mColorSpinner;
+    @BindView(R.id.seasonFilterSpinner) Spinner mSeasonSpinner;
 
     public FilterItemsFragment() {
     }
@@ -28,6 +30,19 @@ public class FilterItemsFragment extends Fragment {
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_filter_items, container, false);
         ButterKnife.bind(this, view);
+
+        WardrobeItem item = new WardrobeItem(getContext());
+
+        mItemSpinner.setAdapter(createAdapter(R.array.item_types_array));
+        mColorSpinner.setAdapter(new ArrayAdapter<String>(getContext(), android.R.layout.simple_spinner_item, item.getAllColors()));
+        mSeasonSpinner.setAdapter(createAdapter(R.array.seasons_array));
         return view;
+    }
+
+    private ArrayAdapter<CharSequence> createAdapter(int array_resource) {
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(getContext(),
+                array_resource, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        return adapter;
     }
 }
