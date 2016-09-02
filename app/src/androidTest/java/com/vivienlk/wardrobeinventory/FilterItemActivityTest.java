7fafe4b7584176fc5d559e967fb4cd836f33451d
viKuo <lost.angel.fin@gmail.com>
2016-09-02 13:38:24 -0500

    cannot figure out why some pages can be tested and not others.

--- app/src/androidTest/java/com/vivienlk/wardrobeinventory/FilterItemActivityTest.java
+++ app/src/androidTest/java/com/vivienlk/wardrobeinventory/FilterItemActivityTest.java
@@ -1,7 +1,29 @@
 package com.vivienlk.wardrobeinventory;
 
+import android.support.test.rule.ActivityTestRule;
+
+import com.vivienlk.wardrobeinventory.UI.FilterItemsActivity;
+
+import org.junit.Rule;
+import org.junit.Test;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+
 /**
  * Created by Vivien on 9/1/2016.
  */
 public class FilterItemActivityTest {
+    @Rule
+    public ActivityTestRule<FilterItemsActivity> activityTestRule =
+            new ActivityTestRule<>(FilterItemsActivity.class);
+
+    @Test
+    public void validateAddItemButton() {
+        onView(withText("AddItemActivity")).check(matches(isDisplayed()));
+    }
 }
